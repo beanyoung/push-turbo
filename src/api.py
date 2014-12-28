00be651d1f6828dee5f548c7a87a5dc98e7b766c
BeanYoung <guobasanxian@gmail.com>
2014-12-28 23:05:54 +0800

    finish api

--- src/api.py
+++ src/api.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import json
+
 import beanstalkc
-from flask import Blueprint, jsonify, g
+from flask import Blueprint, jsonify, g, request
 
 import config
 
@@ -14,14 +16,32 @@ api = Blueprint('api', __name__)
 def before_request():
     g.beanstalk = beanstalkc.Connection(
         host=config.BEANSTALKD_HOST, port=config.BEANSTALKD_PORT)
-    g.beanstalk.watch('push')
 
 
 @api.route('/push', methods=['POST'])
 def push_jobs():
-    pass
+    job = dict()
+    job['app_name'] = request.form.get('app_name')
+    job['payload'] = dict()
+    job['payload']['alert'] = request.form.get('alert')
+    job['payload']['sound'] = request.form.get('sound', 'default')
+    job['payload']['badge'] = int(request.form.get('badge', '1'))
+    job['payload']['custom'] = json.loads(request.form.get('custom', '{}'))
+
+    device_tokens = request.form.get('device_tokens').split(',')
+    if len(device_tokens) < 5:
+        g.beanstalk.watch('push')
+        for device_token in device_tokens:
+            job['device_token'] = device_token
+            g.beanstalk.put(json.dumps(job))
+    else:
+        g.beanstalk.watch('batch_push')
+        job['device_tokens'] = device_tokens
+        g.beanstalk.put(json.dumps(job))
+    return jsonify(dict())
 
 
 @api.route('/push_stats', methods=['GET'])
 def push_stats():
+    g.beanstalk.watch('push')
     return jsonify(g.beanstalk.stats_tube('push'))
